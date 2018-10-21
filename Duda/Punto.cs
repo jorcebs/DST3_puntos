4ed5333eddcea527e798ce5a3cc0dc0814e034b2
jorcebs <jor.cebs@gmail.com>
2018-10-20 20:00:26 -0500

    Upload

---
+++ Duda/Punto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puntos
+{
+    class Punto
+    {
+        public int x;
+        public int y;
+
+        public Punto()
+        {
+            x = y = 5;
+        }
+
+        public Punto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public double calculaDistancia(int x2, int y2)
+        {
+            return Math.Sqrt(Math.Pow(x - x2, 2) + Math.Pow(y - y2, 2));
+        }
+
+        public double calculaDistancia(Punto punto)
+        {
+            return calculaDistancia(punto.x, punto.y);
+        }
+
+        public override string ToString()
+        {
+            return "(" + x.ToString() + "," + y.ToString() + ")";
+        }
+    }
+}
